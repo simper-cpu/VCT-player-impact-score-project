a5bd8e42b8cc426b8b0540cc4dcea0c5c20d56fb
simper-cpu <maiphuhuy123@gmail.com>
2026-08-08 01:02:09 +0700

    them bang

--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,13 @@
-from pathlib import Path 
+from asyncio import events
+from pathlib import Path
+from datetime import datetime
+
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+
+URL =  "https://www.vlr.gg/events/?region=27&tier=60"
+
+OUTPUT_PATH = Path("data/raw/vlr_VCT_EMEA_events_URL_2.csv")
 
 
-URL = "https://www.vlr.gg/event/2976/vct-2026-emea-stage-2"
-
-response = requests.get(URL, timeout=30)
-response.raise_for_status()
-print(response.status_code)
-soup = BeautifulSoup(response.text, "html.parser")
-event_item = soup.find("div", class_ = "event-header-main")
-
-
-date_item = event_item.find(lambda tag: tag.name == "div" and tag.get_text(strip=True) == "Dates")
-date_label = date_item.parent
-date = date_label.find("div", class_="value").get_text(strip=True)
-print(date.replace(" –", ",").split(", "))
